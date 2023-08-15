c1d469a5a983d2f9e743aefe74fe675b32d2631b
ibrahimkultepe <ibrahimkultepe@gmail.com>
2023-08-15 13:38:54 +0300

    first commit

---
+++ TheMovieDatabase/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  TheMovieDatabase
+//
+//  Created by İbrahim Kültepe on 15.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
