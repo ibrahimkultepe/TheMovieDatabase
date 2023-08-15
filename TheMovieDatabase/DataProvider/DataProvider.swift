d2895e32277ec5f0dffe49365d2eb45a3a01c291
ibrahimkultepe <ibrahimkultepe@gmail.com>
2023-08-15 19:03:10 +0300

    add dataprovider

---
+++ TheMovieDatabase/DataProvider/DataProvider.swift
@@ -0,0 +1,9 @@
+//
+//  DataProvider.swift
+//  TheMovieDatabase
+//
+//  Created by İbrahim Kültepe on 15.08.2023.
+//
+
+let apiDataProvider = APIDataProvider(interceptor: APIRequestInterceptor.shared,
+                                      eventMonitors: [])
