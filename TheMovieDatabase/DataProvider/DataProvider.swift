77bb7e4de6bf8fbd2cf17d52f306aa81e5859d8b
ibrahimkultepe <ibrahimkultepe@gmail.com>
2023-08-15 19:23:18 +0300

    Revert "add dataprovider"
    This reverts commit d2895e32277ec5f0dffe49365d2eb45a3a01c291.

--- TheMovieDatabase/DataProvider/DataProvider.swift
+++
@@ -1,9 +0,0 @@
-//
-//  DataProvider.swift
-//  TheMovieDatabase
-//
-//  Created by İbrahim Kültepe on 15.08.2023.
-//
-
-let apiDataProvider = APIDataProvider(interceptor: APIRequestInterceptor.shared,
-                                      eventMonitors: [])
